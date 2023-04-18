a59f1145d65284759379c6b389abbae874bef977
Bhaskar Nelapogula <bnelapogula@evoketechnologies.com>
2023-04-18 18:36:02 +0530

    intial commit

---
+++ src/main/java/com/app/CollectionTasksApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionTasksApplication.class, args);
+	}
+
+}
